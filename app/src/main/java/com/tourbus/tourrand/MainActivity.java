0c09492cdc81127597f2ae0ba1547d4b22eff3c8
moonazn <sjy3729@naver.com>
2024-06-18 04:47:46 +0900

    0618-스플래시 거의완료

--- app/src/main/java/com/tourbus/tourrand/MainActivity.java
+++ app/src/main/java/com/tourbus/tourrand/MainActivity.java
@@ -1,21 +1,30 @@
 package com.tourbus.tourrand;
 
+import static android.util.Base64.encodeToString;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.kakao.sdk.auth.model.OAuthToken;
 import com.kakao.sdk.common.model.AuthErrorCause;
 import com.kakao.sdk.user.UserApiClient;
 import com.kakao.sdk.user.model.User;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import kotlin.Unit;
 import kotlin.jvm.functions.Function1;
@@ -27,8 +36,8 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView id, email;
     private Button logoutBtn;
+
     private ImageView kakaoLoginButton;
-    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,34 +48,46 @@ public class MainActivity extends AppCompatActivity {
         logoutBtn = findViewById(R.id.logout);
         id = findViewById(R.id.semiText);
         email = findViewById(R.id.mainText);
-        progressBar = findViewById(R.id.progress_bar);
 
-        Function2<OAuthToken, Throwable, Unit> callback = new Function2<OAuthToken, Throwable, Unit>() {
+        Log.d("getKeyHash",""+getKeyHash(MainActivity.this));
+
+        Function2<OAuthToken,Throwable, Unit> callback =new Function2<OAuthToken, Throwable, Unit>() {
             @Override
+            // 콜백 메서드 ,
             public Unit invoke(OAuthToken oAuthToken, Throwable throwable) {
-                progressBar.setVisibility(View.GONE);
-                if (throwable != null) {
-                    Log.e(TAG, "로그인 실패", throwable);
-                    showLoginFailedDialog("로그인에 실패했습니다. 다시 시도해주세요.");
-                } else if (oAuthToken != null) {
-                    Log.i(TAG, "로그인 성공");
+                Log.e(TAG,"CallBack Method");
+                //oAuthToken != null 이라면 로그인 성공
+                if(oAuthToken!=null){
+                    // 토큰이 전달된다면 로그인이 성공한 것이고 토큰이 전달되지 않으면 로그인 실패한다.
                     updateKakaoLoginUi();
+
+                }else {
+                    //로그인 실패
+                    Log.e(TAG, "invoke: login fail" );
                 }
+
                 return null;
             }
         };
-
         kakaoLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (UserApiClient.getInstance().isKakaoTalkLoginAvailable(MainActivity.this)) {
-                    UserApiClient.getInstance().loginWithKakaoTalk(MainActivity.this, callback);
-                } else {
-                    UserApiClient.getInstance().loginWithKakaoAccount(MainActivity.this, callback);
-                }
-            }
-        });
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                finish();
 
+//                if(UserApiClient.getInstance().isKakaoTalkLoginAvailable(MainActivity.this)){
+//                    UserApiClient.getInstance().loginWithKakaoTalk(MainActivity.this, callback);
+//                }else{
+//                    // 카카오톡이 설치되어 있지 않다면
+//                    UserApiClient.getInstance().loginWithKakaoAccount(MainActivity.this, callback);
+//                }
+
+            }
+
+
+        });
         logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -79,10 +100,31 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         });
-
         updateKakaoLoginUi();
-    }
 
+
+    }
+    public static String getKeyHash(final Context context){
+        PackageManager pm = context.getPackageManager();
+        try {
+            PackageInfo packageInfo = pm.getPackageInfo(context.getPackageName(),PackageManager.GET_SIGNATURES);
+            if(packageInfo == null)
+                return null;
+
+            for(Signature signature : packageInfo.signatures){
+                try {
+                    MessageDigest md = MessageDigest.getInstance("SHA");
+                    md.update(signature.toByteArray());
+                    return encodeToString(md.digest(), Base64.NO_WRAP);
+                } catch (NoSuchAlgorithmException e){
+                    e.printStackTrace();
+                }
+            }
+        } catch (PackageManager.NameNotFoundException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
     private void showLoginFailedDialog(String message) {
         new AlertDialog.Builder(this)
                 .setTitle("로그인 실패")
@@ -92,28 +134,53 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void updateKakaoLoginUi() {
+
+        // 로그인 여부에 따른 UI 설정
         UserApiClient.getInstance().me(new Function2<User, Throwable, Unit>() {
             @Override
             public Unit invoke(User user, Throwable throwable) {
+
                 if (user != null) {
+
+                    // 유저의 아이디
+                    Log.d(TAG, "invoke: id =" + user.getId());
+                    // 유저의 이메일
+                    Log.d(TAG, "invoke: email =" + user.getKakaoAccount().getEmail());
+                    // 유저의 닉네임
+                    Log.d(TAG, "invoke: nickname =" + user.getKakaoAccount().getProfile().getNickname());
+                    // 유저의 성별
+                    //Log.d(TAG, "invoke: gender =" + user.getKakaoAccount().getGender());
+                    // 유저의 연령대
+                    //Log.d(TAG, "invoke: age=" + user.getKakaoAccount().getAgeRange());
+
+
+                    // 유저 닉네임 세팅해주기
                     id.setText("이름 : " + user.getId().toString());
+                    // 유저 프로필 사진 세팅해주기
                     email.setText("이메일 : " + user.getKakaoAccount().getEmail());
+                    Log.d(TAG, "invoke: profile = "+user.getKakaoAccount().getProfile().getThumbnailImageUrl());
+
+                    // 로그인이 되어있으면
                     kakaoLoginButton.setVisibility(View.GONE);
                     logoutBtn.setVisibility(View.VISIBLE);
-
-                    // 로그인 성공 시 다음 화면으로 이동
                     Intent intent = new Intent(MainActivity.this, HomeActivity.class);
                     startActivity(intent);
                     overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
                     finish();
+
                 } else {
-                    id.setText(null);
-                    email.setText(null);
+                    // 로그인 되어있지 않으면
+                    //id.setText(null);
+                    //email.setText(null);
+
                     kakaoLoginButton.setVisibility(View.VISIBLE);
-                    logoutBtn.setVisibility(View.GONE);
+                    logoutBtn.setVisibility(View.VISIBLE);
+
+
                 }
                 return null;
             }
         });
     }
+
 }
--- app/src/main/java/com/tourbus/tourrand/SplashActivity.java
+++ app/src/main/java/com/tourbus/tourrand/SplashActivity.java
@@ -1,40 +1,38 @@
 package com.tourbus.tourrand;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-
+import android.widget.MediaController;
+import android.widget.VideoView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
-
-    private static final int SPLASH_DISPLAY_DURATION = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-//        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-//        startActivity(intent);
-//        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
-//        finish();
+        VideoView videoView = findViewById(R.id.videoView);
+        Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.splash_white_72030);
 
-        // 애니메이션 적용
-        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.rotate);
-        findViewById(R.id.part2).startAnimation(fadeIn);
+        // MediaController 설정 (옵션)
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
 
-        // 일정 시간 후에 메인 액티비티로 전환
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
-                SplashActivity.this.startActivity(mainIntent);
-                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
-                SplashActivity.this.finish();
-            }
-        }, SPLASH_DISPLAY_DURATION);
+        videoView.setVideoURI(videoUri);
+        videoView.setOnPreparedListener(mp -> {
+            mp.setLooping(false);  // 필요한 경우 루핑 설정
+            videoView.start();
+        });
+
+        videoView.setOnCompletionListener(mp -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out); // 화면 전환 애니메이션 제거
+            finish();
+        });
     }
 }
